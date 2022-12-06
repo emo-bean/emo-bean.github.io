c0f8fedce445f6ab5f2477c298194c3a878afeee
emo-bean <emogene@pdx.edu>
2022-12-06 15:50:07 -0800

    blondiebytes

---
+++ blondiebytes/js/script.js
@@ -0,0 +1,11 @@
+function revealmessage() {
+    document.getElementById("hiddenmessage").style.display = 'block';
+}
+function countdown() {
+    var currentVal = document.getElementById("countDownButton").innerHTML;
+    var newVal = 0;
+    if (currentVal > 0) {
+        newVal = currentVal - 1;
+    }
+    document.getElementById("countDownButton").innerHTML = newVal
+}
